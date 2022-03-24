7b0f96a306e29be4d3f702254b460c9925463b62
CodeBloggerVienna <viktor@blumauer.ch>
2022-03-24 11:36:49 +0100

    003 hinzufügen des API Keys

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApiKey: "sS9jgoiN7vzbihUkpADcaq4UUcAgo1K4"
 };
 
 /*
