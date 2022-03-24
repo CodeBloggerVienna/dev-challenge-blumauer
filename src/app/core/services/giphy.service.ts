5d0b63e509fd7307fbbd46d414610f102effb95f
CodeBloggerVienna <viktor@blumauer.ch>
2022-03-24 13:16:21 +0100

    006 Implementierung der Gif Komponente

--- src/app/core/services/giphy.service.ts
+++ src/app/core/services/giphy.service.ts
@@ -1,18 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GiphyService {
 
+  gifs = new BehaviorSubject<any>([]);
+  
   constructor(private http: HttpClient) { }
 
-
+  searchGifs(input: string){
+    return this.http.get(`${environment.baseUrl}/search?q=${input}&api_key=${environment.giphyApiKey}&limit=10`)
+    .subscribe((res: any)=>{
+        this.gifs.next(res.data);
+    });
+  }
 
   getGifs(){
-    return this.http.get(`${environment.baseUrl}/trending?api_key=%${environment.giphyApiKey}&limit=25`);
+    return this.gifs.asObservable();
   }
 
 }
--- src/app/pages/gif-output/gif-output.component.ts
+++ src/app/pages/gif-output/gif-output.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { GiphyService } from 'src/app/core/services/giphy.service';
 
 @Component({
   selector: 'app-gif-output',
   templateUrl: './gif-output.component.html',
   styleUrls: ['./gif-output.component.css']
 })
-export class GifOutputComponent implements OnInit {
+export class GifOutputComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  gifs: any[] = [];
+  subscription: Subscription | undefined;
+
+  constructor(private dataService : GiphyService) { }
 
   ngOnInit(): void {
+    this.subscription = this.dataService.getGifs()
+    .subscribe((res: any)=>{
+      this.gifs = res;
+    }) 
+  }
+  
+  //
+  ngOnDestroy(){
+    if(this.subscription)
+    this.subscription.unsubscribe();
   }
 
 }
