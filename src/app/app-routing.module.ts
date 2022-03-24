a1cf81538f40edc96a1a0c047d7ab85f7edbeb33
CodeBloggerVienna <viktor@blumauer.ch>
2022-03-24 15:21:36 +0100

    007 Änderung der Routing Funktion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GifOutputComponent } from './pages/gif-output/gif-output.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/gif-output', pathMatch: 'full' },
+  { path: 'gif-output', component: GifOutputComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
