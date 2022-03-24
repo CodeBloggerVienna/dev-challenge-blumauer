217e5ab4bf7d718737d4a8aaad1d35007138a8fd
CodeBloggerVienna <viktor@blumauer.ch>
2022-03-24 15:25:41 +0100

    0011 Finalisierung der Navleiste

--- src/app/shared/headbar/headbar.component.ts
+++ src/app/shared/headbar/headbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FavouriteGiphyService } from 'src/app/core/services/favourite-giphy.service';
+import { GiphyService } from 'src/app/core/services/giphy.service';
 
 @Component({
   selector: 'app-headbar',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeadbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService : GiphyService,private favService: FavouriteGiphyService) { }
 
+  favGifs : any[] = [];
+  
   ngOnInit(): void {
+    this.favService.favGifs.subscribe(
+      res => {
+        this.favGifs = res;
+      }
+    )
   }
+
+  loadGifs(input:string){
+    
+    if(input !== ""){
+      console.log(input);
+      this.dataService.searchGifs(input);
+    }else{
+      this.dataService.searchGifs("search me!");
+    }
+
+  }
+
+  loadFavs(){
+    this.dataService.changeGifs(this.favGifs);
+  }
+
 
 }
